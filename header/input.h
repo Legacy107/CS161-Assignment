d96bfac0f8100739d12bc3789e01bfb00214aa17
Legacy107 <49741920+Legacy107@users.noreply.github.com>
2021-11-01 16:05:11 +0700

    Setup header files

--- header/input.h
+++ header/input.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <vector>
+
+// output instruction: y to start or n to quit
+// get and validate input
+// return true if y and false if n
+bool start_input();
+
+// output instruction and example (A1 -> open, A1F -> flag or unflag)
+// get and validate input (if the cell is already open -> invalid)
+// convert to numbers A->0 1->0
+// if of open type return {{0, 0}, 0}
+// if of flag type return {{0, 0}, 1}
+pair<pair<int, int>, bool> cell_input(int width, int height, vector<vector<int>> &mask);
+
+void input();
