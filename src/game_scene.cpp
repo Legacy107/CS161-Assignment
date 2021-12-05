ba992347b16b5ea6f0dacf754b00cadf204b4eff
Khoa Nguyễn <nldkhoa21@apcs.fitus.edu.vn>
2021-12-05 15:40:45 +0700

    MS7

--- src/game_scene.cpp
+++ src/game_scene.cpp
@@ -4,6 +4,72 @@
 #include <iostream>
 #include <windows.h>
 
+void game_draw(struct minesawyer* game)
+{
+    cout << "Use the arrow keys to move" << endl;
+    cout << "To open a cell, press O" << endl;
+    cout << "To flag a cell, press F" << endl;
+    cout << "To go back, press B " << endl;
+}
+
+bool game_input(struct minesawyer* game)
+{
+    if (GetKeyState(VK_UP) & 0x8000)
+    {
+        if (is_valid_cell(game->cursor.first, game->cursor.second - 1, game->width, game->height))
+        {
+            game->cursor.second -= 1;
+        }
+        return true;
+    }
+    else if (GetKeyState(VK_DOWN) & 0x8000)
+    {
+        if (is_valid_cell(game->cursor.first, game->cursor.second + 1, game->width, game->height))
+        {
+            game->cursor.second += 1;
+        }
+        return true;
+    }
+    else if (GetKeyState(VK_RIGHT) & 0x8000)
+    {
+        if (is_valid_cell(game->cursor.first + 1, game->cursor.second, game->width, game->height))
+        {
+            game->cursor.first += 1;
+        }
+        return true;
+    }
+    else if (GetKeyState(VK_LEFT) & 0x8000)
+    {
+        if (is_valid_cell(game->cursor.first - 1, game->cursor.second, game->width, game->height))
+        {
+            game->cursor.first -= 1;
+        }
+        return true;
+    }
+    else if (GetKeyState('O') & 0x8000)
+    {
+        if (game->mask[game->cursor.first][game->cursor.second] == 0)
+        {
+            if (open_cell(game->cursor.first, game->cursor.second, game->width, game->height, game->board, game->mask))
+                minesawyer::change_scene(MENU);
+        }
+        return true;
+    }
+    else if (GetKeyState('F') & 0x8000)
+    {
+        if (game->mask[game->cursor.first][game->cursor.second] == 0)
+            flag_cell(game->cursor.first, game->cursor.second, game->flags, game->mask);
+        return true;
+    }
+    else if (GetKeyState('B') & 0x8000)
+    {
+        save_board(game->width, game->height, game->mines, game->seed, game->mask);
+        minesawyer::change_scene(MENU);
+        return true;
+    }
+    else return false;
+}
+
 void game_scene(struct minesawyer *game)
 {
     while (true)
--- src/menu_scene.cpp
+++ src/menu_scene.cpp
@@ -4,6 +4,42 @@
 #include <iostream>
 #include <windows.h>
 
+void menu_draw()
+{
+    cout << "  ================MINESWEEPER================" << endl;
+    cout << "                  NEW GAME (N)" << endl;
+    cout << "                  CONTINUE (C)" << endl;
+    cout << "                SCORE BOARD (S)" << endl;
+    cout << "                   EXIT (E)" << endl;
+    cout << "  Press the letter after each option to choose" << endl;
+}
+
+bool menu_input(struct minesawyer* game)
+{
+    if (GetKeyState('N') & 0x8000)
+    {
+        minesawyer::change_scene(CHOOSER);
+        return true;
+    }
+    else if (GetKeyState('C') & 0x8000)
+    {
+        minesawyer::loadboard();
+        minesawyer::change_scene(GAME);
+        return true;
+    }
+    else if (GetKeyState('S') & 0x8000)
+    {
+        minesawyer::change_scene(SCORE);
+        return true;
+    }
+    else if (GetKeyState('E') & 0x8000)
+    {
+        minesawyer::change_scene(EXIT);
+        return true;
+    }
+    else return false;
+}
+
 void menu_scene(struct minesawyer *game)
 {
     while (true)
@@ -16,3 +52,4 @@ void menu_scene(struct minesawyer *game)
         }
     }
 }
+
