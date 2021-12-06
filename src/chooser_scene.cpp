657c9fed2d3d235b8de4a508580d1c120e20a5c1
ngvahieu <93477565+ngvahieu@users.noreply.github.com>
2021-12-06 20:33:00 +0700

    MS6 (#8)
    * saveboard+input
    
    * Refactor, format code and add chooser_draw
    
    * Add default value for seed in create_board
    
    Co-authored-by: Legacy107 <49741920+Legacy107@users.noreply.github.com>

--- src/chooser_scene.cpp
+++ src/chooser_scene.cpp
@@ -2,16 +2,53 @@
 #include "../header/minesawyer.h"
 #include <iostream>
 #include <windows.h>
+#include "game_scene.h"
+
+void chooser_draw()
+{
+    std::cout << "================" << std::endl;
+    std::cout << " Choose a board " << std::endl;
+    std::cout << "================" << std::endl;
+    std::cout << endl;
+
+    for (int i = 0; i < board_options.size(); i++)
+    {
+        std::cout << i + 1 << ". ";
+        cout << board_options[i].name << " " << board_options[i].width
+             << " x " << board_options[i].height << " "
+             << board_options[i].mines << " mines (" << i + 1 << ")" << std::endl;
+    }
+
+    std::cout << std::endl << "< Menu (M)" << std::endl;
+}
+
+bool chooser_input(struct minesawyer *game)
+{
+    for (int i = 0; i < board_options.size(); i++)
+    {
+        if (GetKeyState((char)('1' + i)) & 0x8000)
+        {
+            game->create_board(board_options[i].width, board_options[i].height, board_options[i].mines);
+            game->change_scene(GAME);
+            return true;
+        }
+    }
+
+    if (GetKeyState('M') & 0x8000)
+    {
+        game->change_scene(MENU);
+        return true;
+    }
+
+    return false;
+}
 
 void chooser_scene(struct minesawyer *game)
 {
+    chooser_draw();
     while (true)
     {
-        chooser_draw();
-        while (true)
-        {
-            if (chooser_input(game))
-                return;
-        }
+        if (chooser_input(game))
+            return;
     }
 }
